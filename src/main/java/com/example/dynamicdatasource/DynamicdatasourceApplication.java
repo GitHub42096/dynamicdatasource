dd139829a15d073cbecd5f08183a1acdcb559b0c
zhangjie <420964597@qq.com>
2019-06-17 16:55:34 +0800

    动态数据源切换

--- src/main/java/com/example/dynamicdatasource/DynamicdatasourceApplication.java
+++ src/main/java/com/example/dynamicdatasource/DynamicdatasourceApplication.java
@@ -10,6 +10,7 @@ public class DynamicdatasourceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DynamicdatasourceApplication.class, args);
+        System.out.println("启动");
     }
 
 }
